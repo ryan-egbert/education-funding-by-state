5968be23b41026eb37c8cde1e5612ee34a5a320d
ryan-egbert <ryanegbert@aggiemail.usu.edu>
2020-11-02 22:37:45 -0700

    Updates

--- hw6/public/js/script.js
+++ hw6/public/js/script.js
@@ -1,23 +1,26 @@
-let tooltip = new Tooltip();
+// let tooltip = new Tooltip();
 
-let votePercentageChart = new VotePercentageChart(tooltip);
+// let votePercentageChart = new VotePercentageChart(tooltip);
 
-let tileChart = new TileChart(tooltip);
+// let tileChart = new TileChart(tooltip);
 
-let shiftChart = new ShiftChart();
+// let shiftChart = new ShiftChart();
 
-let electoralVoteChart = new ElectoralVoteChart(shiftChart, tooltip);
+// let electoralVoteChart = new ElectoralVoteChart(shiftChart, tooltip);
 
 
-// Load the data corresponding to all the election years.
-// Pass this data and instances of all the charts that update on year
-// selection to yearChart's constructor.
-d3.csv("data/yearwiseWinner.csv").then(electionWinners => {
-  console.log(electionWinners);
-  let yearChart = new YearChart(electoralVoteChart, tileChart,
-                                votePercentageChart, electionWinners);
-  yearChart.update();
+// // Load the data corresponding to all the election years.
+// // Pass this data and instances of all the charts that update on year
+// // selection to yearChart's constructor.
+// d3.csv("data/yearwiseWinner.csv").then(electionWinners => {
+//   console.log(electionWinners);
+//   let yearChart = new YearChart(electoralVoteChart, tileChart,
+//                                 votePercentageChart, electionWinners);
+//   yearChart.update();
 
-  let s = d3.select('#y2012');
-  yearChart.selectYear(s, s.data()[0]);
-});
+//   let s = d3.select('#y2012');
+//   yearChart.selectYear(s, s.data()[0]);
+// });
+
+
+timeline = new Timeline();
